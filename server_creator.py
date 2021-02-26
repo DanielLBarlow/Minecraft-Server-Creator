1ed1d4b1d8bb4bb2f98e293f043529eeb500323f
DanielLBarlow <58518273+DanielLBarlow@users.noreply.github.com>
2021-02-26 16:43:02 +0000

    Add files via upload

---
+++ server_creator.py
@@ -0,0 +1,67 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+
+
+def website():
+    url = "https://getbukkit.org/download/vanilla"
+    vanillaversions = BeautifulSoup(requests.get(url).text, "html.parser")
+
+    version_categories = vanillaversions.find("div", id="download")
+    version_numbers = version_categories.find_all("h2")
+    version_numbers_text = [version.text for version in version_numbers]
+    return version_numbers_text
+
+
+def user_selection(version_numbers_text):
+    version_input = input(
+        "Please enter a minecraft version number. Or type 'list' to see a list of versions: ")
+    if version_input in version_numbers_text:
+        print("Your server is now being created...")
+
+        url = downloadLink(version_input)
+        r = requests.get(url, allow_redirects=True)
+        open("minecraft-server-" + version_input + ".jar", 'wb').write(r.content)
+
+        makeDir("eula.txt")
+        open("eula.txt", "w").write("eula=true")
+
+        makeDir("start.bat")
+        open("start.bat", "w").write("java -jar " +
+                                     "minecraft-server-" + version_input + ".jar")
+
+        os.popen("java -jar " + "minecraft-server-" +
+                 version_input + ".jar").read()
+
+        # Create server
+    elif version_input == "list":
+        print(version_numbers_text)
+        user_selection(version_numbers_text)
+    else:
+        print("That is an invalid version number. Please try again")
+        user_selection(version_numbers_text)
+
+
+def downloadLink(version):
+    url = "https://getbukkit.org/download/vanilla"
+    vanillaversions = BeautifulSoup(requests.get(url).text, "html.parser")
+    download = vanillaversions.find("div", id="download")
+    downloadpane = download.find_all("div", {"class": "download-pane"})
+    for individualDownloadPane in downloadpane:
+        h2 = individualDownloadPane.find("h2")
+        if h2.getText() == version:
+            url = individualDownloadPane.find(
+                "a", id="downloadr").attrs['href']
+            vanillaversions = BeautifulSoup(
+                requests.get(url).text, "html.parser")
+            paneHref = vanillaversions.find("div", id="get-download")
+            return paneHref.find("a").attrs['href']
+
+
+def makeDir(dir):
+    if not os.path.exists(dir):
+        open(dir, "x")
+
+
+versions = website()
+user_selection(versions)
